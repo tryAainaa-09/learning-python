071bd86c93c36fe3b1d4bb364d4b8cf0683a61a4
tryAainaa-09 <dalila.aainaa@gmail.com>
2026-07-03 17:01:55 +0800

    Add files via upload

---
+++ string_exercise.py
@@ -0,0 +1,57 @@
+print(f"This is the other desktop to use for practice")
+
+# age = 18
+
+# if age >= 18:
+#     print("You are an adult.")
+# else:
+#     print("You are a minor")
+
+############ test score ###################
+# score = int(input("Enter a number : "))
+
+# if score >= 90:
+#     grade = "A"
+# elif score >= 80:
+#     grade = "B"
+# elif score >= 70:
+#     grade = "C"
+# elif score >= 60:
+#     grade = "D"
+# else:
+#     grade = "F"
+
+# print(f"Your grade is: {grade}")
+
+############# test and ####################
+
+# user_age = int(input("What is your age: "))
+# has_license = bool("Do you have license? True or False: ")
+
+# if user_age >= 18 and has_license:
+#     print("You are allowed to drive.")
+# else:
+#     print("You are not allowed to drive.")
+    
+
+############# test or  ####################
+
+# day = str(input("What day is today? : "))
+
+# if day == "Saturday" or day == "Sunday":
+#     print("It's the weekend!")
+# else:
+#     print("It's a weekday.")
+
+############# Exercise 05 ################
+
+print(f"This is a BMI indicator. Let's start\n")
+weight = float(input("Enter your weight (in KG): "))
+height = float(input("Enter your height (in meters): "))
+
+bmi = weight / (height**2)
+
+print(f"Your BMI is {bmi:.2f}")
+
+
+
